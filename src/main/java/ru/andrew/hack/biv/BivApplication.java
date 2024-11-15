dbff8114dc274472cf0faa68e6a16c9c641261d7
Andrew97422 <nosoff.4ndr@yandex.ru>
2024-11-15 19:50:53 +0300

    init

---
+++ src/main/java/ru/andrew/hack/biv/BivApplication.java
@@ -0,0 +1,13 @@
+package ru.andrew.hack.biv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BivApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BivApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/andrew/hack/biv/BivApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.andrew.hack.biv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BivApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
